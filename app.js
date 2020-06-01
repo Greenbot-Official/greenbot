fe1c8e0a1909672d2316a1bb3ee0b028f10b1b81
Toby <tmkoth431@isd709.org>
2020-06-01 11:11:43 -0500

    got rid of everything broken

--- app.js
+++ app.js
@@ -2,107 +2,39 @@ const config = require('./config');
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const { Users, CurrencyShop } = require('./dbObjects');
-const { Op } = require('sequelize');
-const currency = new Discord.Collection();
 const PREFIX = '::';
 
-Reflect.defineProperty(currency, 'add', {
-	value: async function add(id, amount) {
-		const user = currency.get(id);
-		if (user) {
-			user.balance += Number(amount);
-			return user.save();
-		}
-		const newUser = await Users.create({ user_id: id, balance: amount });
-		return newUser;
-	},
-});
-
-Reflect.defineProperty(currency, 'getBalance', {
-	value: function getBalance(id) {
-		const user = currency.get(id);
-		return user ? user.balance : -1;
-	},
-});
-
 client.once('ready', async () => {
-	const storedBalances = await Users.findAll();
-	storedBalances.forEach(b => currency.set(b.user_id, b));
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', async message => {
 	const id = message.author.id;
-	const user = currency.get(id);
-	if (message.author.bot) return;
-	currency.add(id, 1);
+	const messageId = message.id
 	if (!message.content.startsWith(PREFIX)) return;
 	const input = message.content.slice(PREFIX.length).trim();
 	if (!input.length) return;
 	const [, command, commandArgs] = input.match(/(\w+)\s*([\s\S]*)/);
+	if (message.author.bot) return;
 
-	if (command === 'balance' || command === 'bal') {
-		const target = message.mentions.users.first() || message.author;
-		return message.channel.send(`${target.tag} has ${currency.getBalance(target.id)}:apple:`);
-  
-  } else if (command === 'inventory') {
-		const target = message.mentions.users.first() || message.author;
-		const user = await Users.findOne({ where: { user_id: target.id } });
-		const items = await user.getItems();
-		if (!items.length) return message.channel.send(`${target.tag} has nothing!`);
-		return message.channel.send(`${target.tag} currently has ${items.map(t => `${t.amount} ${t.item.name}`).join(', ')}`);
-  
-  } else if (command === 'transfer') {
-		const currentAmount = currency.getBalance(message.author.id);
-		const transferAmount = commandArgs.split(/ +/).find(arg => !/<@!?\d+>/.test(arg));
-		const transferTarget = message.mentions.users.first();
-		if (!transferAmount || isNaN(transferAmount)) return message.channel.send(`Sorry ${message.author}, that's an invalid amount`);
-		if (transferAmount > currentAmount) return message.channel.send(`Sorry ${message.author} you don't have that much.`);
-		if (transferAmount <= 0) return message.channel.send(`Please enter an amount greater than zero, ${message.author}`);
-		currency.add(message.author.id, -transferAmount);
-		currency.add(transferTarget.id, transferAmount);
-		return message.channel.send(`Successfully transferred ${transferAmount}:apple: to ${transferTarget.tag}. Your current balance is ${currency.getBalance(message.author.id)}:apple:`);
-  
-  } else if (command === 'buy') {
-		const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: commandArgs } } });
-		if (!item) return message.channel.send('That item doesn\'t exist.');
-		if (item.cost > currency.getBalance(message.author.id)) {
-			return message.channel.send(`You don't have enough currency, ${message.author}`);
-		}
-		const user = await Users.findOne({ where: { user_id: message.author.id } });
-		currency.add(message.author.id, -item.cost);
-		await user.addItem(item);
-		message.channel.send(`You've bought a ${item.name}`);
-  
-  } else if (command === 'shop') {
-		const items = await CurrencyShop.findAll();
-		return message.channel.send(items.map(i => `${i.name}: ${i.cost}:apple:`).join('\n'), { code: true });
-  
-  } else if (command === 'leaderboard') {
-		return message.channel.send(
-			currency.sort((a, b) => b.balance - a.balance)
-				.filter(user => client.users.has(user.user_id))
-				.first(10)
-				.map((user, position) => `(${position + 1}) ${(client.users.get(user.user_id).tag)}: ${user.balance}:apple:`)
-				.join('\n'),
-			{ code: true }
-		);
-  
-	} else if (command === 'fish') {
-		var rand = Math.abs(Math.round(Math.random()*10));
-		message.channel.send(`you caught a ${rand}in :fish:`);
-		return currency.add(id, rand);
-
-	} else if (command === 'hello') {
+	if (command === 'hello') {
     return message.channel.send("hi, I am greenbot");
   
-  } else if (command === 'help') {
+	} else if (command === 'dm') {
+		const dUser = message.mentions.users.first();
+		if (!dUser) return message.channel.send("Can't find user!")
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't you that command!")
+    mentionMessage = commandArgs;
+    if(mentionMessage.length < 1) return message.reply('You must supply a message!')
+    dUser.send(`${mentionMessage}`)
+		message.author.send(`${message.author} You have sent your message to ${dUser}`)
+
+	} else if (command === 'help') {
 
   } else if (command === 'dummy') {
 		const target = message.mentions.users.first()
 		if (!target) return;
-		return message.channel.send(`${target} is a poopyface`)
+		return message.channel.send(`${target} is a dummy`)
 
 	}
   
